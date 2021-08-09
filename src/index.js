38619ca1d2337d008b66734a67345968b4de9526
Yevgeniy Polyarush <polyarush.e@mail.ru>
2021-08-09 15:04:55 +0400

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+  
+  if (typeof array == 'object' && array.length > 0) {
+    return Math.min.apply(null, array);
+  }
+    return 0
+  
 }
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (typeof array == 'object' && array.length > 0) {
+    return Math.max.apply(null, array);
+  } 
+    return 0
+  }
+
 
 exports.avg = function avg (array) {
+  let total = 0;
+  if (typeof array == 'object' && array.length > 0) {
+    for (let i = 0; i != array.length; i++) {
+      total += array[i]
+    }
+    
+    return (total / array.length)  
+
+  }
   return 0;
 }
